ea0b3fc9b183c5f945e0538a83210d9f2f02fb28
JenChieh <jcs090218@gmail.com>
2024-03-09 23:39:08 -0800

    Impls wip

--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,53 @@
 const std = @import("std");
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+const word_separators = [_]u8{
+    ' ',
+    '-',
+    '_',
+    ':',
+    '.',
+    '/',
+    '\\',
+};
+
+const default_score: i32 = -35;
+
+/// Check if CHAR is a word character.
+fn word(ch: u8) bool {
+    return !std.mem.containsAtLeast(u8, &word_separators, 1, &[_]u8{ch});
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+/// Check if CHAR is an uppercase character.
+fn capital(ch: u8) bool {
+    return word(ch) and std.ascii.isUpper(ch);
+}
+
+/// Check if LAST-CHAR is the end of a word and CHAR the start of the next.
+///
+/// This function is camel-case aware.
+fn boundary(last_ch: u8, ch: u8) bool {
+    if (ch == null) {
+        return false;
+    }
+
+    if (!capital(last_ch) and capital(ch)) {
+        return true;
+    }
+
+    if (!word(last_ch) and word(ch)) {
+        return true;
+    }
+
+    return false;
+}
+
+//--- Testing
+
+test "test word" {
+    try testing.expect(word('a'));
+}
+
+test "test capital" {
+    try testing.expect(capital('A'));
 }
